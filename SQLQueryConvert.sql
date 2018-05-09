8029afe8d3982cfcfbbaf0782e4590dc456203be
ArtAmmirati <34355879+ArtAmmirati@users.noreply.github.com>
2018-05-09 18:43:21 -0400

    Next UPload

--- SQLQueryConvert.sql
+++
@@ -1,5 +0,0 @@
-USE mytable
-INSERT INTO dbo.MemberSort_real (FirstName, LastName, DOB, City, [State],JoinDate)
-SELECT FirstName, LastName, dbo.standard_date_convert(DOB), City, [State],
-		dbo.standard_date_convert(JoinDate)
-FROM dbo.MemberSort_1
